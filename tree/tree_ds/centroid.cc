e8177db682d36c4a9605537abd9a2f6be0681f9f
mrkusss <mrkus428@gmail.com>
2025-08-12 18:30:38 +0200

    adding algorithm that finds centroid in tree

---
+++ tree/tree_ds/centroid.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> childrens[1000001];
+int size[1000001];
+
+int n;
+
+void subSize(int node, int prev)
+{
+    size[node] = 1;
+
+    for (int child : childrens[node])
+    {
+        if (child != prev)
+        {
+            subSize(child, node);
+            size[node] += size[child];
+        }
+    }
+}
+
+int centroid(int node, int prev)
+{
+    for (int child : childrens[node])
+    {
+        if (child != prev && size[child] > n/2)
+        {
+            return centroid(child,node);
+        }
+    }
+
+    return node;
+}
+
+int main()
+{
+    int x, y;
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i)
+    {
+        cin >> x >> y;
+
+        childrens[x].push_back(y);
+        childrens[y].push_back(x);
+    }
+
+    subSize(1,-1);
+    cout << centroid(1,-1);
+    return 0;
+}
